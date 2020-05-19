4cd791a11e03d16d0dfc97347e2d0fcf6fec71f7
mayur nemade <mayurnemade7@gmail.com>
2020-05-19 17:46:52 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native'
+
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
